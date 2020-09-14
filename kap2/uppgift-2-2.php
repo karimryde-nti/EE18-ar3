701765bb7650bcfbebca5285f5063118994d056f
karye <karye.webb@gmail.com>
2020-09-14 14:02:13 +0200

    Fix

--- kap2/uppgift-2-2.php
+++ kap2/uppgift-2-2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SVar från formulär</title>
+    <title>Svar från formulär</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
