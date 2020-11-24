bbcfb45df5abd6421e374785120b93e2ac2c04c7
Karye <karye@users.noreply.github.com>
2020-11-24 15:13:23 +0100

    Ny db-conn

---
+++ db/db-conn.php
@@ -0,0 +1,41 @@
+<?php
+/*
+* PHP version 7
+* @category   Lånekalkylator
+* @author     Karim Ryde <karye.webb@gmail.com>
+* @license    PHP CC
+*/
+
+$user = "karye";
+$db = "karye";
+$host = "localhost";
+$pass = "bt7KD6tsXPohvrz4";
+
+// Logga in på mysql-server och välj databas
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Gick det att ansluta?
+if ($conn->connect_error) {
+    die("Kunde inte ansluta: " . $conn->connect_error);
+} else {
+    echo "<p>Hurrah! Gick bra att ansluta!</p>";
+}
+
+/* // Ställ en sql-fråga
+$sql = "SELECT * FROM bilar";
+$result = $conn->query($sql);
+
+// Gick sql-satsen att köra?
+if (!$result) {
+    die("Något blev fel med sql-satsen");
+} else {
+    echo "<p>Lista på alla bilar kunde hämtas</p>";
+}
+
+// Loopa igenom listan på bilar
+while ($rad = $result->fetch_assoc()) {
+    echo "<p>$rad[marke]</p>";
+}
+
+// Stäng ned anslutningen
+$conn->close(); */
